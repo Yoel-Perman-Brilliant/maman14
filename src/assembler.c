d21766e2d2856c7a5ff02a64a5181ec9c081ed90
Yoel Perman Brilliant <yoyoperman@gmail.com>
2024-08-10 14:00:41 +0300

    documentation

--- src/assembler.c
+++ src/assembler.c
@@ -1,3 +1,31 @@
+/**
+ * This is the main file for an assembler program, which converts a text-base assembly program given in a .as file to
+ * machine code which resides in a newly-created .ob file, as well as addition files that are explained later.
+ * 
+ * The first stage is pre-assembly, which creates a parsed .am file out of the given .as file. The pre-assembly process
+ * is responsible for parsing every macro in the .as file, such that the .am file is macro-less.
+ * The second stage is a double-pass assembly - the assembler goes over  the .am file twice, and translates it to
+ * machine code which is kept in the file's requirements structure. The first pass is responsible for encoding the first
+ * word of each instruction and .data and .string directives, as well as giving a value to every symbol. The second pass
+ * is responsible for the encoding of operands, as well as listing entry symbols and uses of external symbols.
+ * Finally, the output files are created based on the encoding that resides in the requirements.
+ * 
+ * If the assembler identifies an input error in the pre-assembly stage, it will not create the .am file, won't
+ * perform the assembly stage, and start the process for the next file. If it identifies an error in the first or second
+ * pass stage, the output files will not be created and the assembly process will start for the next file.
+ * 
+ * If a memory allocation failure occurs, the program will stop as quickly as possible while freeing any
+ * allocated memory.
+ * 
+ * The input for the program is a list of extensionless file names, each corresponding to a .as file. The extensionless
+ * file names are given as command line arguments. The output for the program includes several output files for each
+ * input files, each with the same name is the input file but a different extension:
+ * 1. A .am file, which is the same as the .as file but all the macros are parsed.
+ * 2. A .ob file, which includes the machine code.
+ * 3. A .ext file, which includes a list of external symbols and the addresses in which they are used.
+ * 4. A .ent file, which includes a list of entry symbols defined in the input file and their values.
+ */
+
 #include "stdio.h"
 #include "../headers/files.h"
 #include "../headers/pre_assembler.h"
@@ -100,6 +128,16 @@ static int assemble(char file_name[]) {
     return 0;
 }
 
+/**
+ * Reads a list of extensions file names from the command line and assembles the corresponding .as files one by one.
+ * See the documentation at the top of the file for more information about the assembly process.
+ * 
+ * @param argc the number of command line arguments (one plus the number of files to be assembled)
+ * @param argv a list of command line arguments (the ./assembler command and the extensionless file names)
+ * @return 0 if all files were assembled successfully, 1 if at least one assembly error occurred, 2 if a memory
+ *         allocation failure occurred (exits with code 2 if necessary in the assemble function), or 3 if no files
+ *         were given.
+ */
 int main(int argc, char **argv) {
     /* whether an assembly error has occurred */
     int failure;
